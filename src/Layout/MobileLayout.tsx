d0394db9165d3347186be1e417356d2dd83c252c
armand.deshais <armand.deshais@siqual.fr>
2023-11-09 16:26:07 +0100

    chore(menu): modifie l'icons "principale"

--- src/Layout/MobileLayout.tsx
+++ src/Layout/MobileLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHome, faClipboard, faTrophy, faPlay} from '@fortawesome/free-solid-svg-icons';
+import {faHome, faTrophy, faPlay} from '@fortawesome/free-solid-svg-icons';
 
 interface MobileLayoutProps {
     children: React.ReactNode;
